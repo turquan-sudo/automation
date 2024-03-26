a9c99741d1547bca4c5c466ca0cf25ed56837269
Jason B <jasonbank@microsoft.com>
2024-03-26 16:12:17 -0600

    Changes to be committed: 	modified:   Group-AzDNS.ps1     Added Warranty clause

--- Group-AzDNS.ps1
+++ Group-AzDNS.ps1
@@ -18,7 +18,12 @@
       .PARAMETER RemoveZone
       This Flag will Remove the Source Zone
   #>
-
+#>
+## ----------------------------------------------------------------------------------
+## THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+## EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+## OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+## ----------------------------------------------------------------------------------
   Param (
     [string]$TargetZone = "",
     [string]$TargetZoneSubscription = "", 
